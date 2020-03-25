5baffa1ab5547b532702515ed0b126dde0df4c14
Moin <anmmoinuddin@gmail.com>
2020-03-25 21:36:16 +0100

    Add files via upload

---
+++ Online Course/Computing in Python I/Computing in Python 1 Part2.py
@@ -0,0 +1,50 @@
+
+
+#In operator in python
+
+myString ="Hello, world"
+print("H" in myString)
+print("lo, w" in myString)
+print("oll" in myString)
+
+# Mathematical Operator
+
+myNum=5
+print(myNum)
+myNum=myNum+3
+print(myNum)
+myNum=myNum-3
+print(myNum)
+myNum=myNum*3
+print(myNum)
+myNum=myNum//3
+print(myNum)
+myNum=myNum**3#exponential 
+print(myNum)
+myNum=myNum%3#modulus
+print(myNum)
+
+#length of string
+
+myString="Hello, world"
+print(myString)
+    #find the length of myString
+myString=str(len(myString))
+print(myString)
+    #find the length again
+myString=str(len(myString))
+print(myString)
+
+
+#Incrementingandloops
+letterCount=0
+
+    #Run this code for each letter in the string
+for character in "Hello, world":
+    #Add one to lettercount
+    letterCount+=1
+print(letterCount)
+
+
+
+
